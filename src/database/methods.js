7517ce2dfd0a1c3a08bac4a253e5ee5e14038adc
adityaraj-09 <itsrishabhforyou@gmail.com>
2024-07-19 17:38:34 +0530

    update

--- src/database/methods.js
+++ src/database/methods.js
@@ -1,7 +1,7 @@
 
 import { auth, db } from "../firebase";  // Import Firestore
-import { collection, getDocs, addDoc, doc, getDoc, setDoc } from 'firebase/firestore';
-import { Patient, Therapist } from "./models";
+import { collection, getDocs, addDoc, doc, getDoc, setDoc, query, where } from 'firebase/firestore';
+import { Excercise, Fee, Patient, Therapist } from "./models";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
@@ -23,7 +23,7 @@ export const findTherapistById=async (id)=>{
         const docRef=doc(db,'therapists',id)
         const docSnap=await getDoc(docRef)
         if(docSnap.exists()){
-                const t=Therapist.fromFireStore(doc)
+                const t=Therapist.fromFireStore(docSnap)
                 return t;
         }
     } catch (error) {
@@ -36,7 +36,7 @@ export const getPatientById =async (id)=>{
         const docRef=doc(db,'patientinfo',id)
         const docSnap=await getDoc(docRef)
         if(docSnap.exists()){
-                const t=Patient.fromFireStore(doc)
+                const t=Patient.fromFireStore(docSnap)
                 return t;
         }
         return null
@@ -99,3 +99,79 @@ export async function signIn(email,password){
         
     }
 }
+
+export async function getPatientsOfTherapyCenter(therapy_centerId){
+    try {
+        const docRef=doc(db,'therapy_centers',therapy_centerId)
+        const docSnap=await getDoc(docRef)
+        const ids=docSnap.patients
+        const patients=[]
+        ids.forEach(id => {
+            const p=getPatientById(id)
+            patients.push(p)
+        });
+        return patients
+        
+    } catch (error) {
+        
+    }
+}
+
+export async function getFeeInfoOfPatient(patient){
+    try {
+        const ref=collection(db,"fees")
+        const q=query(ref,where('patient','==',patient))
+        const querySnapshot=await getDocs(q)
+        const fees=querySnapshot.docs.map((doc)=>Fee.fromFireStore(doc))
+        return fees;
+    } catch (error) {
+        return []
+    }
+    
+        
+}
+
+export const findExcercises=async()=>{
+    try {
+        const querySnapshot = await getDocs(collection(db, 'excercises'));
+        const data = querySnapshot.docs.map((doc)=>Excercise.fromFireStore(doc))
+
+        return data;
+
+    } catch (error) {
+        return []
+    }
+}
+
+export async function addExercise(newExercise){
+    try {
+        const docRef = await addDoc(collection(db, 'exercises'), newExercise);
+        const docId = docRef.id;
+        // Update the newExercise object with the docId
+        newExercise.uid = docId;
+        // Save the newExercise object again with the docId
+        await setDoc(docRef, newExercise);
+    } 
+    
+     catch (error) {
+        
+    }
+}
+
+export async function getPatientsOfTherapist(therapistId){
+    try {
+        const docRef=doc(db,'therapists',therapistId)
+        const docSnap=await getDoc(docRef)
+        const ids=docSnap.patients
+        const patients=[]
+        ids.forEach(id => {
+            const p=getPatientById(id)
+            patients.push(p)
+        });
+        return patients
+        
+    } catch (error) {
+        return []
+    }
+}
+
--- src/database/models.js
+++ src/database/models.js
@@ -1,14 +1,14 @@
 
 
 export class Therapist{
-    constructor(uid,name,phone,gender,city,registration_No,therapist_center,profile,registration_year,qualification,college,experience,patients){
+    constructor(uid,name,phone,gender,city,registration_No,therapy_center,profile,registration_year,qualification,college,experience,patients){
         this.name=name;
         this.uid=uid
         this.phone=phone;
         this.gender=gender;
         this.city=city;
         this.registration_No=registration_No;
-        this.therapist_center=therapist_center;
+        this.therapy_center=therapy_center;
         this.profile=profile;
         this.registration_year=registration_year;
         this.qualification=qualification;
@@ -19,12 +19,12 @@ export class Therapist{
     }
     static fromFireStore(doc){
         const data=doc.data()
-        return new Therapist(doc.id,data.name,data.phone,data.gender,data.city,data.registration_No,data.therapist_center,data.profile,data.registration_year,data.qualification,data.college,data.experience,data.patients)
+        return new Therapist(doc.id,data.name,data.phone,data.gender,data.city,data.registration_No,data.therapy_center,data.profile,data.registration_year,data.qualification,data.college,data.experience,data.patients)
     }
 }
 
 export class Patient{
-    constructor(uid,name,dob,doj,address,remarks,parentPhone,parentNames,profile){
+    constructor(uid,name,dob,doj,address,remarks,parentPhone,parentNames,profile,batch,time){
         this.name=name
         this.dob=dob
         this.uid=uid
@@ -34,15 +34,56 @@ export class Patient{
         this.parentNames=parentNames
         this.parentPhone=parentPhone
         this.profile=profile
+        this.batch=batch
+        this.time=time
+
     }
     static fromFireStore(doc){
         const data=doc.data()
-        return new Patient(doc.id,data.name,data.dob,data.doj,data.address,data.remarks,data.parentPhone,data.parentNames,data.profile)
+        return new Patient(doc.id,data.name,data.dob,data.doj,data.address,data.remarks,data.parentPhone,data.parentNames,data.profile,data.batch,data.time)
     }
 }
 
-export class Fee{
-    constructor(){
+export class TherapyCenter{
+    constructor(uid,name,patients,therapists){
+        this.uid=uid
+        this.name=name
+        this.patients=patients
+        this.therapists=therapists
+    }
 
+    static fromFireStore(doc){
+        const data=doc.data()
+        return new TherapyCenter(doc.id,data.name,data.patients,data.therapists)
+    }
+}
+export class Fee{
+    constructor(uid,for_month,date,amount,patient){
+        this.uid=uid
+        this.for_month=for_month
+        this.date=date
+        this.amount=amount
+        this.patient=patient
+    }
+    static fromFireStore(doc){
+        const data=doc.data()
+        return new Fee(doc.id,data.for_month,data.date,data.amount,data.patient)
+    }
+}
+
+export class Excercise{
+    constructor(uid,level,level_desc,excercise,remarks,description,file,content_type){
+        this.uid=uid
+        this.level=level
+        this.level_desc=level_desc
+        this.excercise=excercise
+        this.remarks=remarks
+        this.description=description
+        this.file=file
+        this.content_type=content_type
+    }
+    static fromFireStore(doc){
+        const data=doc.data()
+        return new Excercise(doc.id,data.level,data.level_desc,data.excercise,data.remarks,data.description,data.file,data.content_type)
     }
 }
